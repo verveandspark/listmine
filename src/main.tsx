085635df10d80aa21ae3844b72ea1aa0a3f72116
Pritish J <jadhav.pritish@gmail.com>
2025-10-14 10:32:47 -0400

    Remove Tempo Routes and Tempo Devtools

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,6 @@ import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
-import { TempoDevtools } from "tempo-devtools";
-TempoDevtools.init();
-
 const basename = import.meta.env.BASE_URL;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
